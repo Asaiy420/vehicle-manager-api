041ea96a043871af705774bf99717eb75b97a15b
Asaiy420 <kcaryan05@gmail.com>
2025-11-18 09:31:06 +0545

    Fix vehicle API route handlers by qualifying vehicle module functions

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,16 @@
-use axum::{Json, Router, routing::get, debug_handler};
+use axum::{ Router, routing::get};
 use tokio::net::TcpListener;
-use serde::Serialize;
+
+mod vehicle;
 
 #[tokio::main]
 
 async fn main(){
     let router01: Router = Router::new()
-        .route("/vehicle", get(get_vehicle).post(post_vehicle));
+        .route("/vehicle", get(vehicle::get_vehicle).post(vehicle::post_vehicle));
     
     let address: &str = "0.0.0.0:6570";
     let listener: TcpListener = tokio::net::TcpListener::bind(address).await.unwrap();
     axum::serve(listener, router01).await.unwrap();
 
-}
-#[derive(Serialize)]
-struct Vehicle{
-    manufacturer: String,
-    model: String,
-    year: u32,
-    id: String,
-}
-
-#[debug_handler]
-async fn get_vehicle() -> Json<Vehicle>{
-    Json::from(
-    Vehicle{
-        manufacturer: String::from("Triumph"),
-        model: String::from("Triumph Scrambler 400x"),
-        year: 2024,
-        id: uuid::Uuid::new_v4().to_string(),
-    })
-}
-
-
-async fn post_vehicle(){
-    
 }
