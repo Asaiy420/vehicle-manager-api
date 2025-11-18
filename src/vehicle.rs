a4e6f7e2ea0998b7a73118cce4266a4755c3d122
Asaiy420 <kcaryan05@gmail.com>
2025-11-18 09:31:12 +0545

    Implement vehicle API with GET and POST route handlers

---
+++ src/vehicle.rs
@@ -0,0 +1,28 @@
+
+use axum::{Json, debug_handler};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Vehicle{
+    
+    manufacturer: String,
+    model: String,
+    year: u32,
+    id: String,
+}
+
+#[debug_handler]
+pub async fn get_vehicle() -> Json<Vehicle>{
+    Json::from(
+    Vehicle{
+        manufacturer: String::from("Triumph"),
+        model: String::from("Triumph Scrambler 400x"),
+        year: 2024,
+        id: uuid::Uuid::new_v4().to_string(),
+    })
+}
+
+
+pub async fn post_vehicle(){
+    
+}
